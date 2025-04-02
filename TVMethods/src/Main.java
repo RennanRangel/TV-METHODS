4010a51e5ff11da3993f564389d058f653596dbe
RennanRangel <rennano965@gmail.com>
2025-04-02 13:22:09 -0300

    Add files via upload

---
+++ TVMethods/src/Main.java
@@ -0,0 +1,11 @@
+        public static void main(String[] args) {
+
+        TV tcl = new TV();
+
+
+        tcl.volume = 20;
+
+        // Exibindo o valor do volume
+        System.out.println("O volume atual é: " + tcl.volume);
+    }
+
